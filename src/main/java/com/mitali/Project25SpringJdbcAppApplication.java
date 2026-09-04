acae3c4911aef33295f4e050b768b6dce545fe49
Mitali01-Java <mitali.yadav.java@gmail.com>
2026-09-04 18:36:36 +0530

    Initial commit : Learnt SpringJdbc -JDBCTemplate Example

---
+++ src/main/java/com/mitali/Project25SpringJdbcAppApplication.java
@@ -0,0 +1,13 @@
+package com.mitali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project25SpringJdbcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project25SpringJdbcAppApplication.class, args);
+	}
+
+}
